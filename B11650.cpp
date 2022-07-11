53c92dd966777e6d5a6a7d55d7ab2a7468088f3c
HYEONSIK OH <ks06891@naver.com>
2022-07-11 19:31:45 +0900

    BaekJoon 11650 좌표 내림차순으로 정렬하기(vector 이용)

---
+++ B11650.cpp
@@ -0,0 +1,36 @@
+// 백준 11650 좌표 내림차순으로 정렬하기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if (a.first == b.first) return a.second < b.second;
+	else return a.first < b.first;
+
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num;
+	cin >> num;
+	
+	vector<pair<int, int>> pos;
+	int x, y;
+
+	for (int i = 0; i < num; i++) {
+		cin >> x;
+		cin >> y;
+
+		pos.push_back(make_pair(x, y));
+	}
+
+	sort(pos.begin(), pos.end(), compare);
+
+	for (int i = 0; i < num; i++) cout << pos[i].first << " " << pos[i].second << "\n";
+	
+	return 0;
+}
