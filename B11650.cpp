3780e49e0260a97d7eb3fe9286a3d8fec195f2cc
HYEONSIK OH <ks06891@naver.com>
2022-07-11 20:20:40 +0900

    BaekJoon 11650 좌표 내림차순으로 정렬하기(vector 이용)

--- B11650.cpp
+++ B11650.cpp
@@ -3,11 +3,10 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 bool compare(pair<int, int> a, pair<int, int> b) {
 	if (a.first == b.first) return a.second < b.second;
 	else return a.first < b.first;
-
-	
 }
 
 int main() {
