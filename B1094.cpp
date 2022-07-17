62c014d3e100e41968aea65eced4d40ace1613a4
HYEONSIK OH <ks06891@naver.com>
2022-07-17 17:53:22 +0900

    BaekJoon 1904 막대기를 계속 반으로 잘라서 몇개의 막대기가 필요해야 X랑 같아지는지...

--- B1094.cpp
+++ B1094.cpp
@@ -1,8 +1,10 @@
+// 백준 1904 막대기를 계속 반으로 잘라서 몇개의 막대기가 필요해야 X랑 같아지는지...
+// 찾아보니까 문제에 숨은 의미가 있었다.
+// 물론 막대기 길이로 찾아도 되지만, 2진수로 바꿔서 1의 갯수를 찾는 문제...;
+
 #include <iostream>
 #include <vector>
 using namespace std;
-int stick_length = 64;
-int count = 1;
 
 int main() {
 	ios_base::sync_with_stdio(false);
