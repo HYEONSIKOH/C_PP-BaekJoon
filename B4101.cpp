8791ff1f8c46040214073ceb8d2e39b6573f4b94
HyeonSik <100890581+HYEONSIKOH@users.noreply.github.com>
2023-01-15 00:12:00 +0900

    BaekJoon 4101 [수학]  작냐?

---
+++ B4101.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    
+    while (a != 0 || b != 0) {
+        if (a > b) cout << "Yes\n";
+        else cout << "No\n";
+        
+        cin >> a >> b;
+    }
+    
+    return 0;
+}
