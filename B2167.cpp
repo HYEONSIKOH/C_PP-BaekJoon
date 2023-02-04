cb2bdf2883eb4ba9a7c54162a3ad8c46df5552fb
HyeonSik <100890581+HYEONSIKOH@users.noreply.github.com>
2023-02-04 20:36:59 +0900

    BaekJoon 2167 [누적합] 2차원 배열의 합

---
+++ B2167.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int psum[1025][1025];
+
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+	int N, M, num;
+
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> num;
+			psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + num;
+		}
+	}
+    
+    int case_cnt;
+    cin >> case_cnt;
+
+	for (int i = 1; i <= case_cnt; i++) {
+		int x1, y1, x2, y2;
+		cin >> y1 >> x1 >> y2 >> x2;
+
+		int ans = psum[y2][x2] - psum[y1 - 1][x2] - psum[y2][x1 - 1] + psum[y1 - 1][x1 - 1];
+		cout << ans << '\n';
+	}
+
+	return 0;
+}
